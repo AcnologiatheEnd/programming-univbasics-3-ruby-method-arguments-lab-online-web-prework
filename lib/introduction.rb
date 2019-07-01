39bf7a0817313bf8dbbaf952a89d26b8e14436b5
winston augustus chung <18.chung.winston@ctkprep.org>
2019-07-01 04:56:38 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,3 +3,8 @@ def introduction(name)
   
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+def introduction_with_language_optional(name,language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  
+introduction_with_language_optional("winston")
